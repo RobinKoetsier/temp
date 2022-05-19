3699a201177a6f6dae5e1e3613ab03bab8e032f2
Robin Koetsier <45625446+RobinKoetsier@users.noreply.github.com>
2022-05-19 15:22:00 +0200

    Add files via upload

---
+++ cats/R/test_function.R
@@ -0,0 +1,13 @@
+#' Load a Matrix
+#'
+#' This function loads a file as a matrix. It assumes that the first column
+#' contains the rownames and the subsequent columns are the sample identifiers.
+#' Any rows with duplicated row names will be dropped with the first one being
+#' kepted.
+#'
+#' @param infile Path to the input file
+#' @return A matrix of the infile
+#' @export
+test_function = function(text = "Hello World!"){
+ print(text) 
+}
